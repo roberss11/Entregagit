c2fa9a958882079ecede4f627df2a89010ef231c
roberss11 <robertoss11d@gmail.com>
2020-11-20 19:20:39 +0100

    Mi commit

---
+++ hp02.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>Harry Porlotes y La Clave Secreta del Wifi</title>
+ </head>
+ <body>
+<?php echo '<h1>Harry Porlotes y La Clave Secreta del Wifi</h1>'; ?>
+<?php print("<p>Desde que en Poniente 2.0 hubo un bug, la web de la escuela se encuentra asediada por un virus contra el que Harry y sus amigos deberán luchar.</p><p>En esta aventura, Harry se enterará de nuevos detalles sobre su destino.</p>
+<p>Como el malvado hacker Ransomwaremort le metió un troyano, en su propio código podría esconderse malware.</p>");?>
+ </body>
+</html>
---
+++ hp03.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>Harry Porlotes y El Prisionero del Firewall</title>
+ </head>
+ <body>
+<?php echo '<h1>Harry Porlotes y El Prisionero del Firewall</h1>'; ?>
+<?php print("<p>Un supuesto hacker maligno se salta el Firewall de la escuela y siembra el kernel panic.</p>
+<p>Pero resulta que el prisionero es familia de Harry, y, que es de los buenos.</p>
+<p>Sabius, así se llama el prisionero, sabe Programación Avanzada y usa atajos de teclado, será el padrino de Harry y le enseñará trucos de programación con PHP y JS.</p>");?>
+ </body>
+</html>
